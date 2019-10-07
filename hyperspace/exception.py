2aaaae67ebdcf5c24f5347a1fdf10552b85aedd8
Young, Todd <youngmt1@ornl.gov>
2019-10-07 16:47:10 -0400

    Add parameter parser for command line
    Hyperspace needs to run from the command line given some parameter
    files from Candle. This parser should be the easiest way to go from
    text files to Hyperspace.

---
+++ hyperspace/exception.py
@@ -0,0 +1,8 @@
+class HyperspaceException(Exception):
+    """ Top-level Vel exception """
+    pass
+
+
+class HyperspaceInitializationException(HyperspaceException):
+    """ Exception during system initialization """
+    pass
